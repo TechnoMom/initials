9dc262283b6971670566d0f536355a9e729a6a1a
Cyn Armistead <TechnoMom@users.noreply.github.com>
2022-02-13 11:45:05 -0500

    Create initials.swift

---
+++ initials.swift
@@ -0,0 +1,10 @@
+//Cyn A
+//I was featured as a guest on Oprah in 1998.
+print(" CCC   L         A")
+print("C   C  L        A  A")
+print("C      L       A    A")
+print("C      L      A A  A A")
+print("C      L      A      A")
+print("C      L      A      A")
+print("C   C  L      A      A")
+print(" CCC   LLLLL  A      A")
